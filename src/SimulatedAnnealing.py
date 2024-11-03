1863ec09331553b82227a93aba0ccc99f9fa4d05
Haikalin <haikalassyauqi70@gmail.com>
2024-11-03 21:08:05 +0700

    simmulated finish

--- src/SimulatedAnnealing.py
+++ src/SimulatedAnnealing.py
@@ -7,10 +7,13 @@ class SimulatedAnnealing:
         """
         Constructor kelas SimulatedAnnealing
         """
-        self.temperature = 4
-        self.alpha = 0.99999
-        self.min_temperature = 0.0009
+        self.temperature = 1
+        self.alpha_cold = 0.99999
+        self.alpha = 0.9999
+        self.min_temperature = 0.00001
         self.iteration = 0
+        self.curretValue = [] 
+        self.probability = 0.3
 
     def getTemperature(self) -> float:
         """
@@ -20,6 +23,7 @@ class SimulatedAnnealing:
         float: suhu saat ini
         """
         return self.temperature
+    
     
     def setTemperature(self, temperature: float) -> None:
         """
@@ -32,42 +36,48 @@ class SimulatedAnnealing:
 
     def simulatedAnnealing(self, current: MagicCube) -> MagicCube:
         random_successor = MagicCube()
-        x = 0
         no_improvement_steps = 0
-        sameRes = 0
-        start_Line = False
-        lastScore = 0
-
+        self.curretValue.append(current.value)
         while self.getTemperature() > self.min_temperature:
             random_successor = current.randomSuccessor()
             delta = random_successor.value - current.value
 
             if delta >= 0:
                 current = random_successor
-                lastScore = current.value2point0()
                 if delta == 0:
                     no_improvement_steps += 0
                 else :
                     no_improvement_steps = 0
             else:
                 probability = 2.71828 ** (delta / self.getTemperature())
-                if probability > 0.8:
-                    lastScore = current.value2point0()
+                if probability > self.probability:
                     current = random_successor
                     no_improvement_steps = 0  # Reset counter on acceptance
                 else:
                     no_improvement_steps += 1
 
-            self.setTemperature(self.getTemperature() * self.alpha)
-                
+            self.curretValue.append(current.value)
             self.iteration += 1    
 
             # self.temperature *= self.alpha
 
+            
+            if no_improvement_steps // 2000 > 0:
+                if no_improvement_steps // 2000 > 0:
+                    minus = no_improvement_steps // 2000
+                elif no_improvement_steps // 2000 > 6:
+                    minus = 2.9
+                if current.value < -50 and self.temperature > 0.001:
+                    self.probability = 0.3 + 0.05 * (1 - minus)
+                self.temperature = self.temperature * self.alpha
+            else:
+                self.probability = 0.3
+                self.temperature = self.temperature * self.alpha_cold
+                
+
             if current.value == 0 :
-                return current
+                return current, self.curretValue, self.iteration
 
 
-        return current
+        return current, self.curretValue, self.iteration
 
-    
